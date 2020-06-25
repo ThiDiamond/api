154afdfe0d836b42bca25420b6528dc359c1274f
Thiago Santana <thi.dimond1@gmail.com>
2020-06-25 17:20:45 -0300

    Add config for atlas mongoDB

--- databaseConfig.js
+++ databaseConfig.js
@@ -1,18 +1,9 @@
 const mongoose = require('mongoose');
 
-const {
-  DB_HOST,
-  NODE_ENV,
-  DB_PORT,
-  DB_USER,
-  DB_PASS,
-  DB_NAME,
-  MONGO_URL,
-} = process.env;
+const { DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME, MONGO_URL } = process.env;
 const url =
-  NODE_ENV === 'test'
-    ? MONGO_URL
-    : `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
+  MONGO_URL ||
+  `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
 
 module.exports = async () => {
   try {
