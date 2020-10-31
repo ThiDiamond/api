f18ab6b77ac5d59f69486f45d8752921eb25216d
Thiago Santana <thi.dimond1@gmail.com>
2020-10-31 00:57:40 -0300

    Add buildbox CRUD

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -3,13 +3,13 @@ const Post = mongoose.model('Post');
 
 module.exports = {
   async index(req, res) {
-    const Posts = await Post.find();
-    return res.json(Posts);
+    const posts = await Post.find();
+    return res.json(posts);
   },
   async store(req, res) {
     try {
-      const Post = await Post.create(req.body);
-      return res.status(201).json(Post);
+      const post = await Post.create(req.body);
+      return res.status(201).json(post);
     } catch (error) {
       return res.status(400).send();
     }
@@ -17,8 +17,8 @@ module.exports = {
 
   async destroy(req, res) {
     try {
-      const Post = await Post.findById(req.params.id);
-      await Post.remove();
+      const post = await Post.findById(req.params.id);
+      await post.remove();
       return res.status(204).send();
     } catch (error) {
       return res.status(404).send();
