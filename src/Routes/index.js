32011be1971fcc5876b19ae80006b61fa6feed79
Thiago Santana <thi.dimond1@gmail.com>
2020-10-31 00:31:44 -0300

    Add buildbox CRUD

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routes = express.Router();
 
-routes.use(require('/buildbox', require('./buildbox')));
+routes.use('/buildbox', require('./buildbox'));
 routes.use(require('./jwt'));
 
 module.exports = routes;
